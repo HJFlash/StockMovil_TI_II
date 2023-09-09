412566eeb355602a892c70f32d3d6c61e0f97a76
DPBascur <danielpa423@gmail.com>
2023-09-09 21:48:39 +0000

    Proyecto V0.0.4 (Inicio de sesion[TEST])

---
+++ TEST/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Inicio from './components/Inicio';
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-fondo w-full">
+        <Inicio/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
