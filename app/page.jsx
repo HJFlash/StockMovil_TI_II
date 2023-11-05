7fa989b43db16877c51701af3a578f9ca8a3aa75
Carlos HJ <chuenumanj@gmail.com>
2023-11-05 18:59:07 -0300

    Proyecto V0.8.1 (Se comentan varios Import sin utilizar)
    En el proyecto se pueden encontrar varios import que se utilizan pero también se encuentran algunos pocos que no se utilizan en ningún momento, espero.

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import LoginForm from "@/components/LoginForm";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+//import { getServerSession } from "next-auth";
+//import { redirect } from "next/navigation";
+//import { authOptions } from "./api/auth/[...nextauth]/route";
 import './styles/App.css'
 export default async function Home() {
   
--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { redirect } from "next/navigation";
+//import { redirect } from "next/navigation";
 
 import Link from "next/link";
 import { useState } from "react";
