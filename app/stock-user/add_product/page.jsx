11e6529d224e1f05cd402214274d9cfd9dd3e81e
DPBascur <danielpa423@gmail.com>
2023-11-18 22:26:02 +0000

    TEST 1

--- app/stock-user/add_product/page.jsx
+++ app/stock-user/add_product/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function addproduct(){
+export default function Addproduct(){
     const [error, setError] = useState(null);
 
     const handleSubmit = async (e) => {
