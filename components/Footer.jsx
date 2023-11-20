a9484f2b0e0d8fd34a4c8d33e1fcdb7eba8d1e64
DPBascur <danielpa423@gmail.com>
2023-11-20 23:00:52 +0000

    Proyecto V1.0.0 Early Access(Se agregaron animaciones/Cambio de logo)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,19 +1,20 @@
-import Logo from "../img/logoStock.png"
+import LogoStock from "../img/LogoMini.png"
 import Image from "next/image"
+import Palabras from "./Palabras"
 
 const Footer = () => {
   return (
     <footer className="bg-black font-sans font-bold py-6 fixed bottom-0 w-full">
-        
         <div className="grid grid-cols-3 max-sm:grid-cols-1 gap-4">
-            <div className="col-span-1 p-4 max-sm:hidden">
+            <div className="col-span-1 max-sm:hidden flex justify-center items-center text-xl">
+                <Palabras/>
             </div>
             <div className="col-span-1 p-4">
                 <div className="bg-gray-300 rounded-full text-center">
                     <Image
-                        src={Logo}
+                        src={LogoStock}
                         alt="Logo"
-                        className="p-1 mx-auto h-14 w-auto"
+                        className="p-1 mx-auto h-16 w-auto"
                     />
                 </div>
             </div>
