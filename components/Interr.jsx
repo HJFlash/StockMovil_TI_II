ad45865788d0877bb706c94d43bdbc68bee0ac6e
DPBascur <danielpa423@gmail.com>
2023-11-21 01:40:26 +0000

    DELETE

--- components/Interr.jsx
+++
@@ -1,4 +0,0 @@
-
-export default function Interr (){
-  return (0)
-}
--- middleware.js
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = { 
-    matcher: ["/stock-user","/stock-admin","/stock-admin/new-user","/stock-admin/edit-user","/stock-user/add_product",
-              "/stock-user/categories","/stock-user/edit_product","/stock-user/edit-perfil"] 
-};//
